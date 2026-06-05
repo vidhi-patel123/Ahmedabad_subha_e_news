ac94d5ec06f0e198508ce658c4e95220870cfd77
unknown <patelvidhi8420@gmail.com>
2026-06-05 11:34:21 +0530

    Add

--- ahmedabadproject/settings.py
+++ ahmedabadproject/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-yzw096!m(5f&hc6n4ch%!ai1loa4tg358iez)-zu0r8(v$tw0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com",
+                 "localhost",
+                 "127.0.0.1",
+                 ]
 
 
 # Application definition
