be81e48fac38e7d4ea6e57df976886a4eb064d4e
unknown <patelvidhi8420@gmail.com>
2026-06-05 11:42:03 +0530

    add staticfiles

--- ahmedabadproject/settings.py
+++ ahmedabadproject/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'ahmedabadproject.urls'
@@ -131,3 +132,5 @@ import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
